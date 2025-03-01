e9ee99c9494d7a5931c426395213b1641691b192
harsh8423 <harsh857498@gmail.com>
2025-03-01 08:20:59 +0530

    added 2nd match

---
+++ client/src/utilities/114967_TreeData.js
@@ -0,0 +1,216 @@
+export const mockTreeData = [
+  {
+    "label": "Sunrisers Hyderabad",
+    "value": "sunrisers-hyderabad",
+    "children": [
+      {
+        "label": "Bowler",
+        "value": "sunrisers-hyderabad-bowler",
+        "children": [
+          {
+            "label": "Jaydev Unadkat",
+            "value": 6327
+          },
+          {
+            "label": "Mohammed Shami",
+            "value": 7909
+          },
+          {
+            "label": "Pat Cummins",
+            "value": 8095
+          },
+          {
+            "label": "Harshal Patel",
+            "value": 8175
+          },
+          {
+            "label": "Adam Zampa",
+            "value": 8642
+          },
+          {
+            "label": "Rahul Chahar",
+            "value": 12087
+          },
+          {
+            "label": "Simarjeet Singh",
+            "value": 14163
+          }
+        ]
+      },
+      {
+        "label": "Batsman",
+        "value": "sunrisers-hyderabad-batsman",
+        "children": [
+          {
+            "label": "Travis Head",
+            "value": 8497
+          },
+          {
+            "label": "Sachin Baby",
+            "value": 8709
+          },
+          {
+            "label": "Abhinav Manohar",
+            "value": 10499
+          },
+          {
+            "label": "Atharva Taide",
+            "value": 13914
+          },
+          {
+            "label": "Aniket Verma",
+            "value": 1447065
+          }
+        ]
+      },
+      {
+        "label": "WK-Batsman",
+        "value": "sunrisers-hyderabad-wk-batsman",
+        "children": [
+          {
+            "label": "Heinrich Klaasen",
+            "value": 10209
+          },
+          {
+            "label": "Ishan Kishan",
+            "value": 10276
+          }
+        ]
+      },
+      {
+        "label": "Batting Allrounder",
+        "value": "sunrisers-hyderabad-batting-allrounder",
+        "children": [
+          {
+            "label": "Abhishek Sharma",
+            "value": 12086
+          },
+          {
+            "label": "Nitish Reddy",
+            "value": 14701
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "label": "Rajasthan Royals",
+    "value": "rajasthan-royals",
+    "children": [
+      {
+        "label": "WK-Batsman",
+        "value": "rajasthan-royals-wk-batsman",
+        "children": [
+          {
+            "label": "Sanju Samson",
+            "value": 8271
+          },
+          {
+            "label": "Dhruv Jurel",
+            "value": 14691
+          }
+        ]
+      },
+      {
+        "label": "Bowler",
+        "value": "rajasthan-royals-bowler",
+        "children": [
+          {
+            "label": "Sandeep Sharma",
+            "value": 8356
+          },
+          {
+            "label": "Tushar Deshpande",
+            "value": 11307
+          },
+          {
+            "label": "Jofra Archer",
+            "value": 11540
+          },
+          {
+            "label": "Kumar Kartikeya",
+            "value": 14190
+          },
+          {
+            "label": "Fazalhaq Farooqi",
+            "value": 14282
+          },
+          {
+            "label": "Akash Madhwal",
+            "value": 15727
+          },
+          {
+            "label": "Maheesh Theekshana",
+            "value": 18504
+          },
+          {
+            "label": "Kwena Maphaka",
+            "value": 23346
+          }
+        ]
+      },
+      {
+        "label": "Batsman",
+        "value": "rajasthan-royals-batsman",
+        "children": [
+          {
+            "label": "Nitish Rana",
+            "value": 9204
+          },
+          {
+            "label": "Shimron Hetmyer",
+            "value": 9789
+          },
+          {
+            "label": "Shubham Dubey",
+            "value": 19328
+          },
+          {
+            "label": "Vaibhav Suryavanshi",
+            "value": 51791
+          }
+        ]
+      },
+      {
+        "label": "Bowling Allrounder",
+        "value": "rajasthan-royals-bowling-allrounder",
+        "children": [
+          {
+            "label": "Wanindu Hasaranga",
+            "value": 10926
+          },
+          {
+            "label": "Yudhvir Singh Charak",
+            "value": 15749
+          }
+        ]
+      },
+      {
+        "label": "Batting Allrounder",
+        "value": "rajasthan-royals-batting-allrounder",
+        "children": [
+          {
+            "label": "Riyan Parag",
+            "value": 12305
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "label": "undefined",
+    "value": "undefined",
+    "children": [
+      {
+        "label": "Undefined",
+        "value": "undefined-undefined",
+        "children": [
+          {
+            "label": "Yashasvi Jaiswal",
+            "value": 13940
+          }
+        ]
+      }
+    ]
+  }
+];
